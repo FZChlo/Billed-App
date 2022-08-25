a4aa6930a3c9f5ad4994b280d96d2f6ec5f9ca4e
FZChlo <fzchlo@gmail.com>
2022-08-25 18:17:12 +0200

    Updates soutenance

--- Billed-app-FR-Front/src/__tests__/Dashboard.js
+++ Billed-app-FR-Front/src/__tests__/Dashboard.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+ 
 import {fireEvent, screen, waitFor} from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
 import DashboardFormUI from "../views/DashboardFormUI.js"
